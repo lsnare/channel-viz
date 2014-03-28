13305fca2b6ad6a7edffd2ffdd8491270f8c2883
Lucian Snare <lsnare1@gmail.com>
2014-03-28 10:56:07 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [1855457926], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'WeatherRock', // Replaces Xively logo in the header
 		dataDuration	= '24hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 300, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
 		dataColor		= '87A96B', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
